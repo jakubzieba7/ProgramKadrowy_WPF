086632608b0f535cfc3eaff31e238e31684f73e1
Jakub Zięba <jakubzieba7@gmail.com>
2023-04-28 21:52:50 +0200

    Declared OnPropertyChanged

--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,10 @@ namespace ProgramKadrowy_WPF.ViewModels
     internal class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
