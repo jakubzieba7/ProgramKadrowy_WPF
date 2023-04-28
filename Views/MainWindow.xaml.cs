e6bda727d9a63d709c9f89a20af69b19282af662
Jakub Zięba <jakubzieba7@gmail.com>
2023-04-28 21:48:50 +0200

    MVVM sketch

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ProgramKadrowy_WPF
+namespace ProgramKadrowy_WPF.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
