a8b8322f420c6cd27b4ffa00f1b36243b0c05cb8
Jakub Zięba <jakubzieba7@gmail.com>
2023-04-29 16:56:23 +0200

    Design of SQLSettingsView.xaml

---
+++ Views/SQLSettingsView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ProgramKadrowy_WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProgramKadrowy_WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for SQLSettingsView.xaml
+    /// </summary>
+    public partial class SQLSettingsView : Window
+    {
+        public SQLSettingsView()
+        {
+            InitializeComponent();
+            DataContext = new SQLSettingsViewModel();
+        }
+    }
+}
