b3b507c4fad2cbde583e659074952983c937b371
Jakub Zięba <jakubzieba7@gmail.com>
2023-04-29 17:19:03 +0200

    MainViewModel commands

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using ProgramKadrowy_WPF.Commands;
+using ProgramKadrowy_WPF.Properties;
+using ProgramKadrowy_WPF.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ProgramKadrowy_WPF.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        public MainViewModel()
+        {
+            AddEmployeeCommand = new RelayCommand(AddEditEmployeeData);
+            EditEmployeeCommand = new RelayCommand(AddEditEmployeeData, CanEditEmployeeData);
+            RefreshEmployeeCommand = new RelayCommand(RefreshEmployeeData);
+            SQLSettingsCommand = new RelayCommand(AddEditSQLSettings);
+        }
+        
+        public ICommand SQLSettingsCommand { get; set; }
+        public ICommand RefreshEmployeeCommand { get; set; }
+        public ICommand EditEmployeeCommand { get; set; }
+        public ICommand AddEmployeeCommand { get; set; }
+
+        private void AddEditEmployeeData(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        private bool CanEditEmployeeData(object obj)
+        {
+            return true;
+        }
+        private void RefreshEmployeeData(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        private void AddEditSQLSettings(object obj)
+        {
+            var addEditSQLSettingsWindow = new SQLSettingsView();
+            addEditSQLSettingsWindow.Closed += AddEditSQLSettingsWindow_Closed;
+            addEditSQLSettingsWindow.ShowDialog();
+        }
+
+        private void AddEditSQLSettingsWindow_Closed(object sender, EventArgs e)
+        {
+            Settings.Default.Save();
+        }
     }
 }
