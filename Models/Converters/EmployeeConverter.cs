3d1ffea200ac7815fd06ee200d57876057a1e120
Jakub Zięba <jakubzieba7@gmail.com>
2023-05-06 13:52:42 +0200

    GetEmployees method

---
+++ Models/Converters/EmployeeConverter.cs
@@ -0,0 +1,30 @@
+﻿using ProgramKadrowy_WPF.Models.Domains;
+using ProgramKadrowy_WPF.Models.Wrappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ProgramKadrowy_WPF.Models.Converters
+{
+    public static class EmployeeConverter
+    {
+        public static EmployeeWrapper ToWrapper(this Employee model)
+        {
+            return new EmployeeWrapper
+            {
+                Id = model.Id,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Comments = model.Comments,
+                Salary = model.Salary,
+                EmploymentDate = model.EmploymentDate,
+                UnemploymentDate = model.UnemploymentDate,
+                IsCurrentlyHired = model.IsCurrentlyHired,
+                Contract = new ContractWrapper { Id = model.Contract.Id, Name = model.Contract.Name }
+            };
+        }
+    }
+}
--- Repository.cs
+++ Repository.cs
@@ -1,9 +1,9 @@
-﻿using ProgramKadrowy_WPF.Models.Domains;
-using System;
+﻿using ProgramKadrowy_WPF.Models.Converters;
+using ProgramKadrowy_WPF.Models.Domains;
+using ProgramKadrowy_WPF.Models.Wrappers;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace ProgramKadrowy_WPF
 {
@@ -14,6 +14,20 @@ namespace ProgramKadrowy_WPF
             using (var context = new ApplicationDBContext())
             {
                 return context.Contracts.ToList();
+            }
+        }
+
+        public List<EmployeeWrapper> GetEmployees(int contractId)
+        {
+            using (var context = new ApplicationDBContext())
+            {
+                var employees = context.Employees.Include(x => x.Contract).AsQueryable();
+
+                if (contractId != 0)
+                    employees = employees.Where(x => x.ContractId == contractId);
+
+                return employees.ToList().Select(x => x.ToWrapper()).ToList();
+
             }
         }
     }
